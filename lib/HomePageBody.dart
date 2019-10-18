af94e386829839d8d73ac9b0fa3c655ffe738565
woojea <dbsdnwp1004@naver.com>
2019-10-18 16:56:46 +0900

    ListView -> Sliver 변경

--- lib/HomePageBody.dart
+++ lib/HomePageBody.dart
@@ -5,11 +5,24 @@ import 'PlanetRow.dart';
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemBuilder: (context, index) => PlanetRow(planets[index]),
-        itemCount: planets.length,
-        padding: EdgeInsets.symmetric(vertical: 16.0),
+    return new Expanded(
+      child: Container(
+        color: Color(0xFF736AB7),
+        child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: EdgeInsets.symmetric(vertical: 24.0),
+              sliver: SliverFixedExtentList(
+                itemExtent: 152.0,
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => PlanetRow(planets[index]),
+                  childCount: planets.length,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
