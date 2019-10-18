0d6cfd1ca194bb3fa8dda3a8975aff53bb6f7b8d
woojea <dbsdnwp1004@naver.com>
2019-10-18 15:50:21 +0900

    Initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:planets_flutter/HomePageBody.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'treva',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w600,
+            fontSize: 36.0,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          HomePageBody(),
+        ],
+      ),
+    );
+  }
+}
