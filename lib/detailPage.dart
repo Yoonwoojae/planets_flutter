62885ec1f794bd807680adad64ab10e1e37387f8
woojea <dbsdnwp1004@naver.com>
2019-10-21 15:01:16 +0900

    클래스 추가 및 로직추가

--- lib/detailPage.dart
+++ lib/detailPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:planets_flutter/model/Planet.dart';
+import 'Style.dart';
+import 'separator.dart';
+import 'PlanetSummary.dart';
 
 class DetailPage extends StatelessWidget {
   final Planet planet;
@@ -11,22 +14,92 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         constraints: BoxConstraints.expand(),
-        color: new Color(0xFF736AB7),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        color: Color(0xFF736AB7),
+        child: new Stack(
           children: <Widget>[
-            Text(planet.name),
-            Hero(
-              tag: "planet-hero-${planet.id}",
-              child: Image.asset(
-                planet.image,
-                width: 96.0,
-                height: 96.0,
-              ),
-            )
+            _getBackground(planet),
+            _getGradient(),
+            _getContent(planet),
+            _getToolbar(context),
           ],
         ),
       ),
     );
   }
 }
+
+Container _getBackground(Planet planet) {
+  return Container(
+    child: Image.asset(
+      planet.backgroundImg,
+      fit: BoxFit.cover,
+      height: 300.0,
+    ),
+    constraints: BoxConstraints.expand(height: 300.0),
+  );
+}
+
+Container _getGradient() {
+  return Container(
+    margin: EdgeInsets.only(top: 190.0),
+    height: 110.0,
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: <Color>[Color(0x00736AB7), Color(0xFF736AB7)],
+        stops: [0.0, 0.9],
+        begin: FractionalOffset(0.0, 0.0),
+        end: FractionalOffset(0.0, 1.0),
+      ),
+    ),
+  );
+}
+
+Widget _getContent(Planet planet) {
+  final _overviewTitle = "Overview".toUpperCase();
+  return new ListView(
+    padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+    children: <Widget>[
+      PlanetSummary(
+        planet,
+        horizontal: false,
+      ),
+      Container(
+        padding: new EdgeInsets.symmetric(horizontal: 32.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Text(
+              _overviewTitle,
+              style: headerTextStyle(),
+            ),
+            Separator(),
+            Text(planet.description),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Container _getToolbar(BuildContext context) {
+  return new Container(
+    margin: new EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+    child: new BackButton(color: Colors.white),
+  );
+}
+
+/*
+Column(
+mainAxisAlignment: MainAxisAlignment.center,
+children: <Widget>[
+Text(planet.name),
+Hero(
+tag: "planet-hero-${planet.id}",
+child: Image.asset(
+planet.image,
+width: 96.0,
+height: 96.0,
+),
+)
+],
+),*/
